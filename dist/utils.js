4e5c1bac40afe9e37af69322a7552a2e1ae41a46
François Guérin <francois.guerin@gravity.earth>
2021-09-13 20:49:19 +0200

    add dist folder

--- dist/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.buildPath = void 0;
-function buildPath(dircap, subdirs = [], filename) {
-    return subdirs.length ? `${dircap}/${subdirs.join('/')}/${filename}` : `${dircap}/${filename}`;
-}
-exports.buildPath = buildPath;
-//# sourceMappingURL=utils.js.map
